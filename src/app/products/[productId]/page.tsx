cd6dd6e4443919840530b8c2694b6d5f6149c215
Thelmo-SM <toraphebi@gmail.com>
2025-07-14 23:08:53 -0400

    fwdf

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,17 +1,39 @@
+// import { notFound } from 'next/navigation';
+// import ProductDetails from '@/features/products/components/ProductDetails';
+// // import { getProducts } from '@/actions';
+// import type { ProductType } from '@/types/product';
+// import { mockProducts } from '@/utils/data/arrayProducts';
+
+// type Props = {
+//   params: {
+//     productId: string;
+//   };
+// };
+
+// export default async function ProductPage({ params }: Props) {
+//   const products: ProductType[] = mockProducts //await getProducts();
+//   const product = products.find((p) => p.id === params.productId);
+
+//   if (!product) return notFound();
+
+//   return (
+//     <ProductDetails product={product} products={products} />
+//   );
+// }
+
 import { notFound } from 'next/navigation';
 import ProductDetails from '@/features/products/components/ProductDetails';
-import { getProducts } from '@/actions';
 import type { ProductType } from '@/types/product';
 import { mockProducts } from '@/utils/data/arrayProducts';
 
-type Props = {
+export default async function ProductPage({
+  params,
+}: {
   params: {
     productId: string;
   };
-};
-
-export default async function ProductPage({ params }: Props) {
-  const products: ProductType[] = mockProducts //await getProducts();
+}) {
+  const products: ProductType[] = mockProducts;
   const product = products.find((p) => p.id === params.productId);
 
   if (!product) return notFound();
